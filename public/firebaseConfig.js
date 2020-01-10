db8849ef947de97a34573352520f74ab5d562763
ErlendLygre <erlend.tl2@gmail.com>
2020-01-10 11:17:52 +0100

    add firebase auth basic logic

---
+++ public/firebaseConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyArVeKMRKCed8bRBln-3oKIi2NfWyM-p2U",
+  authDomain: "tjattvik-kommune.firebaseapp.com",
+  databaseURL: "https://tjattvik-kommune.firebaseio.com",
+  projectId: "tjattvik-kommune",
+  storageBucket: "tjattvik-kommune.appspot.com",
+  messagingSenderId: "79979585331",
+  appId: "1:79979585331:web:14eaa52d44e38bdeff6846",
+  measurementId: "G-RM8H3ESBTE"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
