bfbc17631eb8d050024cd0464b852b790cf915b0
ErlendLygre <erlend.tl2@gmail.com>
2020-01-10 11:50:50 +0100

    add simple routing based on login status

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-let userName = '........';
+let generatedUserName = '........';
 const displayName = document.getElementById("name")
 const joinChatButton = document.getElementById("join-chat-button")
 const landingContent = document.getElementById("landing-content")
@@ -12,7 +12,6 @@ function getRandomNames(allTheNames) {
   return randomNames
 }
 
-//----remember error-handling
 const fetchNames = async () => {
   const response = await fetch("https://data.ssb.no/api/v0/no/table/10501")
   const results = await response.json()
@@ -21,39 +20,51 @@ const fetchNames = async () => {
 
 const startNameGenerator = async() => {
   let names = await fetchNames()
-  userName = names[names.length-1]
+  generatedUserName = names[names.length-1]
   for (let i=0; i<40; i++) {
     timer(i, names[i])
   }
   function timer(i, name) { 
    setTimeout(function() { 
     displayName.innerHTML = name
-    if (userName === name) joinChatButton.style.display = 'block'
+    if (generatedUserName === name) joinChatButton.style.display = 'block'
    }, 1 * i * i ) 
  }
 }
 
 function startChat() {
+  //Sign in new user anonymously
   firebase.auth().signInAnonymously().catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
+    alert(error.code + ' | ' + error.message)
+  })
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-      // User is signed in.
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      // ...
+      //Set the current user's display name to the generatedUserName
+      var current = firebase.auth().currentUser;
+      current.updateProfile({
+        displayName: generatedUserName,
+      }).then(function() {
+        // Simple "router". Chat is shown when user is successfully
+        // logged in and assigned the generated name
+        landingContent.style.display = 'none'
+        chatContent.style.display = 'flex'
+      }).catch(function(error) {
+        alert('Kunne ikke oppdatere brukeren med nytt navn')
+      });
     } else {
-      // User is signed out.
-      // ...
+      // Chat cannot be shown when user doesn't exist
+      chatContent.style.display = 'none'
+      landingContent.style.display = 'flex'
     }
-    // ...
   });
-  landingContent.style.display = 'none'
-  chatContent.style.display = 'flex'
+}
+
+function endSession() {
+  firebase.auth().currentUser.delete().then(function() {
+    startNameGenerator()
+  }).catch(function(error) {
+    alert('Kunne ikke forlate Tjattvik. Du må bli her for alltid')
+});
 }
 
 startNameGenerator()
